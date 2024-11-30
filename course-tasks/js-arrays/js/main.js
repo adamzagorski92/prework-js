da0ae40f8a23294d2e94cac103942ad9aac94378
adamzagorski92 <kontakt@adamzagorski.pl>
2024-11-30 09:06:46 +0100

    arrays: done

---
+++ course-tasks/js-arrays/js/main.js
@@ -0,0 +1,17 @@
+let num1= 100;
+let num2= 2;
+
+let array = [1, 100, num1, num1*num2];
+
+console.log(array);
+
+/* Dodatkowe, chciałem sprawdzić kilka rzeczy */
+
+console.log("Wartość dla indexu 0: ", array[0]);
+console.log("Wartość dla indexu 1: ", array[1]);
+console.log("Wartość dla indexu 2: ", array[2]);
+console.log("Wartość dla indexu 3: ", array[3]);
+
+let array2 =[array[0]*2,array[1]*2,array[2]*2,array[3]*2];
+
+console.log(array2);
