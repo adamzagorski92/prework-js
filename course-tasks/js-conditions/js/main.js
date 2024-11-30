1ced42b133a546cdfa69911f2ced84c00873863d
adamzagorski92 <kontakt@adamzagorski.pl>
2024-11-30 10:04:57 +0100

    conditions: done

---
+++ course-tasks/js-conditions/js/main.js
@@ -0,0 +1,8 @@
+let trollStrength = 12;
+let ratStrength = 10;
+
+if(trollStrength > ratStrength) {
+    console.log("Siła Trola jest większa od siły szczura xD")
+} else {
+    console.log("Siła Trola nie jest większa od siły szczura - szczur na sterydach xD")
+}
