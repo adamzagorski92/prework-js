067359e015252bd7706c92eab40dcd8f52f8e7bc
adamzagorski92 <kontakt@adamzagorski.pl>
2024-11-30 09:44:10 +0100

    arrays with variables: done

---
+++ course-tasks/js-arrays-with-variables/js/main.js
@@ -0,0 +1,9 @@
+let today = new Date();
+
+let day = today.getDate();
+let month = today.getMonth()+1;
+let year = today.getFullYear();
+
+let dateData = [day, month, year];
+
+console.log(dateData);
