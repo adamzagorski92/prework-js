3d0ed8c0fcb71490a832ea53565e072fdaf024f3
adamzagorski92 <kontakt@adamzagorski.pl>
2024-11-30 08:46:02 +0100

    variable declaration: Done

---
+++ course-tasks/js-konsola/js/main.js
@@ -0,0 +1,9 @@
+/* Pierwszy sposób */
+
+console.log("Uwielbiam JavaScript");
+
+/* Drugi sposób - pobieram za pomocą metody identyfikacji obiektu za pomocą ID */
+let htmlParagraph = document.getElementById('myParagraph').textContent;
+
+console.log(htmlParagraph);
+
---
+++ course-tasks/js-konsola/readme.txt
@@ -0,0 +1,2 @@
+Próbowałem pobrać tekst z HTML ze znacznika p za pomocą metody:
+querySelector('p').innerContent; natomiast to nie działało - dlaczego?
